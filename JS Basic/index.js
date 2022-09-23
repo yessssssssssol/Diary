58a8d6f1d3d3112fa8fd82d0a18ea020655d5283
Hailey_ys <whassup_man@naver.com>
2022-09-23 15:58:11 +0900

    first commit

---
+++ JS Basic/index.js
@@ -0,0 +1,12 @@
+console.log(area1());
+// console.log(area2());
+
+//함수 선언식 => 호이스팅 가능
+function area1() {
+  return '안녕하세요';
+}
+
+//함수 표현식 => 호이스팅 불가능
+const area2 = function () {
+  return 'hi';
+};
